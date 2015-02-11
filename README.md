aaaef7a947ff351602c8a2d3ae8144ca9ed202c3
Abel Lopez <abelopez@cisco.com>
2015-02-10 19:29:18 -0800

    Added exception handling
    Gleek was crashing out is there was anything funky about the image
    This now gives up on it and proceeds. Also added readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # gleek
 Glance Peek
+
+Are you using Ceph (rbd) for your glance backend?
+Do you wish that you could easily inspect glance images that are stored up there?
+
+Well, you've come to the right place.
+
+Gleek takes a Peek inside Glance.
+All you need is your admin openrc, set the name of your rbd client, and we're off to the races.
+
+Usage:
+source openrc
+export RBD_CLIENT=your-glance-client
--- gleek.py
+++ gleek.py
@@ -22,25 +22,29 @@ glance = glclient.Client(glance_endpoint, token=keystone.auth_token)
 imagelist = glance.images.list()
 for image in imagelist:
     uuid = image['id']
-    label = image['name']
+    name = image['name']
     location = image['direct_url']
     format = image['disk_format']
     url = urlparse(location)
     protocol = url.scheme
-    disk = url.path.strip("/")
-    disk = disk.strip("/snap")
+    disk = url.path.lstrip("/")
+    disk = disk.rstrip("/snap")
 # Todo, how do I figure out the username? Maybe load from cfg?
     username = env['RBD_CLIENT']
     g = guestfs.GuestFS(python_return_dict=True)
-    g.add_drive_opts(disk, 1, format, label=label,
-                     protocol=protocol, username=username)
+    print "Checking %(protocol)s for %(name)s" % locals()
+    try:
+        g.add_drive_opts(disk, 1, format,
+                         protocol=protocol, username=username)
+    except RuntimeError as msg:
+        print "%s (ignored)" % msg
     g.launch()
     roots = g.inspect_os()
     for root in roots:
-        print "Product name: %s" % (g.inspect_get_product_name(root))
+        print "Product name for %s: %s" % (name,
+                                           g.inspect_get_product_name(root))
         print "Version: %d.%d" % (g.inspect_get_major_version(root),
                                   g.inspect_get_minor_version(root))
         print "Type: %s" % (g.inspect_get_type(root))
         print "Distro: %s" % (g.inspect_get_distro(root))
-    g.remove_drive(label)
     g.close()
