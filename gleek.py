1596d03d5f0acfc9f0c6e20d372d8d04144895ca
Abel Lopez <abelopez@cisco.com>
2015-02-20 23:06:47 -0800

    added glance metadata
    Added os_distro and os_version in accordance to glance docs
    http://docs.openstack.org/developer/glance/common-image-properties.html
    This adds names like 'linux' or 'windows' to glance metadata

--- gleek.py
+++ gleek.py
@@ -4,7 +4,8 @@
 # Glance + Peek = gleek
 
 import argparse
-import glanceclient.v2.client as glclient
+from glanceclient import Client as glclient
+import glanceclient.v2.client as gl2client
 import guestfs
 import keystoneclient.v2_0.client as ksclient
 from os import environ as env
@@ -19,13 +20,18 @@ c.execute('''CREATE TABLE IF NOT EXISTS images(
              id text, name text, product text, version text, type text,
              distro text)''')
 
+auth_url = ""
+username = ""
+password = ""
+tenant_name = ""
 
-def inspect_image(disk, uuid, name, format, protocol, username):
+
+def inspect_image(disk, uuid, name, format, protocol, rbd_client):
     g = guestfs.GuestFS(python_return_dict=True)
     print "Checking %(protocol)s for %(name)s" % locals()
     try:
         g.add_drive_opts(disk, 1, format,
-                         protocol=protocol, username=username)
+                         protocol=protocol, username=rbd_client)
     except RuntimeError as msg:
         print "%s (ignored)" % msg
         return
@@ -42,6 +48,7 @@ def inspect_image(disk, uuid, name, format, protocol, username):
     c.execute("INSERT INTO images VALUES (?,?,?,?,?,?)",
               (uuid, name, product, version, ostype, distro))
     conn.commit()
+    update_image(uuid, ostype, version)
 
 
 def report_images():
@@ -53,7 +60,7 @@ def report_images():
                                                        img[3], img[4])
 
 
-def get_imagelist(auth_url, username, password, tenant_name):
+def get_imagelist():
     # Figure out endpoints
     keystone = ksclient.Client(auth_url=auth_url,
                                username=username,
@@ -61,7 +68,7 @@ def get_imagelist(auth_url, username, password, tenant_name):
                                tenant_name=tenant_name)
 
     glance_endpoint = keystone.service_catalog.url_for(service_type='image')
-    glance = glclient.Client(glance_endpoint, token=keystone.auth_token)
+    glance = gl2client.Client(glance_endpoint, token=keystone.auth_token)
     imagelist = glance.images.list()
     for image in imagelist:
         uuid = image['id']
@@ -73,14 +80,27 @@ def get_imagelist(auth_url, username, password, tenant_name):
         disk = url.path.lstrip("/")
         disk = disk.rstrip("/snap")
         # Todo, how do I figure out the username? Maybe load from cfg?
-        username = env['RBD_CLIENT']
+        rbd_client = env['RBD_CLIENT']
         # check db if we already have this image, else get it
         exists = c.execute("SELECT id FROM images where id='%s'" % uuid)\
                   .fetchone()
         if exists is None:
-            inspect_image(disk, uuid, name, format, protocol, username)
+            inspect_image(disk, uuid, name, format, protocol, rbd_client)
         else:
             print "Image %(name)s already inspected, skipping" % locals()
+
+
+def update_image(img_id, img_os, img_ver):
+    # Figure out endpoints
+    keystone = ksclient.Client(auth_url=auth_url,
+                               username=username,
+                               password=password,
+                               tenant_name=tenant_name)
+
+    glance_endpoint = keystone.service_catalog.url_for(service_type='image')
+    glance = glclient('1', glance_endpoint, token=keystone.auth_token)
+    options = {'properties': {'os_distro': img_os, 'os_version': img_ver}}
+    glance.images.update(img_id, **options)
 
 
 def parse_args():
@@ -112,22 +132,26 @@ def parse_args():
         print "OS_TENANT_NAME not set, did you source openrc?"
         raise
     try:
-        parser.add_argument('rbd_client_name', help='Cephx user',
+        parser.add_argument('--rbd_client_name', help='Cephx user',
                             default=env['RBD_CLIENT'])
     except KeyError:
         print "RBD_CLIENT not set"
         raise
 
     args = parser.parse_args()
+    global auth_url
     auth_url = args.auth_url
+    global username
     username = args.os_username
+    global password
     password = args.os_password
+    global tenant_name
     tenant_name = args.os_tenant_name
 
     if args.command == 'report':
         report_images()
     elif args.command == 'check':
-        get_imagelist(auth_url, username, password, tenant_name)
+        get_imagelist()
         report_images()
 
 
